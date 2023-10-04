a6a306c86994ba2975498a150d1a22f0c0127e24
Abdullah <a.az119@hotmail.com>
2023-10-04 10:53:16 +0300

    update

--- myaccount.js
+++
@@ -1,9 +0,0 @@
-// let main_1 = document.getElementById('div-h3')
-// let main = document.getElementById("h5")
-// let second = document.getElementById('h5_2')
-// if(main, second) {
-//     let second2 = 40 - main
-//     let second3 = document.createElement('h5')
-//     main_1.appendChild(second3)
-//     second3.textContent = second2
-// }
