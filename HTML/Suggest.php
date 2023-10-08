0c1336a5aa0ab0e7404e885746674c5d2944e0cb
Abdullah <abdullah.alresaini@gmail.com>
2023-10-08 20:43:46 +0300

    need some work on the nav

--- HTML/Suggest.php
+++ HTML/Suggest.php
@@ -14,6 +14,7 @@ if(is_log_in() == '') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../CSS/Homepage.css">
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-bold-rounded/css/uicons-bold-rounded.css'>
@@ -26,12 +27,13 @@ if(is_log_in() == '') {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css
     ">
     <script src="https://kit.fontawesome.com/f775d5945e.js" crossorigin="anonymous"></script>
-
     <link rel="stylesheet" href="../CSS/Suggest.css">
     <title>اقتراح | المنصة التطوعية</title>
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg sticky-top" id="nav" dir='rtl'>
+
+  <nav class="navbar navbar-expand-lg sticky-top no-padding" id="nav" dir="rtl">
+    <div class="container">
         <a class="navbar-brand" href="#">
             <img src="../IMG/logo.png" alt="شعار ثانوية الشيخ عبدالعزيز بن باز" width="140" height="70">
         </a>
@@ -41,7 +43,7 @@ if(is_log_in() == '') {
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="../HTML/Homepage.php" id="active">الرئيسية</a>
+                    <a class="nav-link" href="#" id="active">الرئيسية</a>
                 </li>
                 <li class="nav-item border-left">
                     <a class="nav-link" href="../HTML/Chanses.php" id="nav2">الفرص التطوعية</a>
@@ -60,6 +62,7 @@ if(is_log_in() == '') {
                             if (is_log_in() != "") {
                         ?>
                         <a class="dropdown-item" href="../php/logout.php" style='text-align:center;'>تسجيل خروج</a>
+                        <a class="dropdown-item" href="../HTML/myaccount.php" style='text-align:center;'>حسابي</a>
                         <?php
                             } else {
                         ?>
@@ -71,44 +74,48 @@ if(is_log_in() == '') {
                 </li>
             </ul>
         </div>
+    </div>
 </nav>
-   
-    
     <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-        <div class="form">
+      <div class="form">
         <div class="contact-info">
           <h3 class="title">! خير الكلام ماقل ودل</h3>
           <p class="text">
            سيتم النظر في اقتراحكم والرد عليه بأسرع وقت        </p>
         </div>
 
-            <div class="contact-form">
-              <form action="index.html" autocomplete="off">
-                <h3 class="title">نسعد باقتراحك</h3>
-                <div class="input-container">
-                <input type="text" name="name" class="input" dir="rtl" placeholder="الاسم الثلاثي"/>
-                </div>
-                <div class="input-container">
-                <input type="text" name="phone" class="input" dir="rtl" placeholder="رقم الهاتف" maxlength="10" />
-                </div>
-                <div class="input-container">
-                <textarea name="message" class="input" dir="rtl" maxlength="200" placeholder="الفرصة المقترحة"></textarea>
-                </div>
-                <input type="submit" value="أرسال" id="btn" />
-              </form>
+        <div class="contact-form">
+
+          <form action="index.html" autocomplete="off">
+            <h3 class="title">نسعد باقتراحك</h3>
+            <div class="input-container">
+              <input type="text" name="name" class="input" dir="rtl" placeholder="الاسم الثلاثي"/>
+              <label for=""></label>
+              <span> اسم المستخدم</span>
             </div>
-          </div>
+            <div class="input-container">
+              <input type="text" name="phone" class="input" dir="rtl" placeholder="رقم الهاتف" maxlength="10" />
+              <label for=""></label>
+              <span> رقم الجوال</span>
+            </div>
+            <div class="input-container textarea">
+              <textarea name="message" class="input" dir="rtl" maxlength="200" placeholder="الفرصة المقترحة"></textarea>
+              <label class="lab" for=""></label>
+              <span> الرسالة</span>
+            </div>
+            <input type="submit" value="أرسال" id="btn" />
+          </form>
         </div>
       </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="../JS/nav.js"></script>
+    <script src="/JS/nav.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js
+    "></script>
+    
     <script>
         AOS.init();
     </script>
